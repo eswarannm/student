08b4cd6ddfccf5b4d0a9c6ac9d6fbeb4011985b7
eswarannm <32105465+eswarannm@users.noreply.github.com>
2017-09-20 22:00:21 -0600

    add blood column

--- students.rb
+++ students.rb
@@ -1,12 +1,15 @@
+
 students = []
 eye_colors = []
 ages = []
+blood_types = []
 
 File.open("student_data.csv").each do |line|
 	info = line.split(",")
 	students.push(info[0].strip)
 	eye_colors.push(info[1].strip)
 	ages.push(info[2].strip.to_i)
+	blood_types.push(info[3].strip)
 end
 
 
